6b17ca4318577f79ff4ae3f5937094dd83b68d5c
Krzysztof Żuraw <services@kzuraw.com>
2021-01-15 08:09:21 +0100

    feat: add working svelte

---
+++ snowpack.config.js
@@ -0,0 +1,17 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: "/",
+    src: "/dist",
+  },
+  plugins: ["@snowpack/plugin-svelte"],
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
